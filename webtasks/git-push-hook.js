bafc8a10d9040158e39d03d42ac7e7a7e25616bf
n4ch03 <iesmite@gmail.com>
2015-11-21 16:58:39 -0300

    First Test of a WebTask from raw url

---
+++ webtasks/git-push-hook.js
@@ -0,0 +1,8 @@
+
+module.exports =
+    function (context, req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(require('ejs').render(view.stringify(), {
+            name: context.data.name || 'Anonymous'
+        }));
+    }
