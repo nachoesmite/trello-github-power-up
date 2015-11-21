919ee9a18d9f18e998f6207cf1e909dac1232680
n4ch03 <iesmite@gmail.com>
2015-11-21 17:15:00 -0300

    Checking what Github is sending

--- webtasks/git-push-hook.js
+++ webtasks/git-push-hook.js
@@ -3,10 +3,21 @@ module.exports =
     function (context, req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(require('ejs').render(view.stringify(), {
-            name: context.data.name || 'Anonymous'
+            name: JSON.stringify(req) || 'Anonymous',
+            name2: JSON.stringify(context) || 'Anonymous 2'
         }));
     }
 
 function view() {
-  
+  /*
+    <html>
+    <head>
+      <title>Welcome to Webtasks</title>
+    </head>
+    <body>
+      <%= name %>
+      <%= name2 %>
+    </body>
+    </html>
+*/
 }
